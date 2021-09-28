323531c0a5a404022320e3ae96a64e1bf2b918a6
Albin Matson Gyllang <albingyllang@gmail.com>
2021-09-28 13:23:33 +0200

    Update CardView.jsx

--- client/components/card/CardView.jsx
+++ client/components/card/CardView.jsx
@@ -2,7 +2,7 @@ import React from "react"
 //component meant to display to display drink cards and perhaps other things aswell
 const CardView = ({imgUrl,text}) => {
     return (
-        <div>
+        <div style={{background:'gray',width:"250px", border:"black 2px solid", borderRadius:"5px"}}>
             <img style={{width:"200px",height:"200px"}} src={ imgUrl ? imgUrl : "https://alladrinkar.se/wp-content/uploads/2021/06/sex-on-the-beach-drink-500x375.png"}/> 
             <div>
                 {text ? text : 'lorem lorem ipsum' }
