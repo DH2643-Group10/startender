60474ad9cb713dca558df23d676cdb13a6d146b9
Albin Matson Gyllang <albingyllang@gmail.com>
2021-10-21 18:12:38 +0200

    styling login/signup

--- client/components/login/SignupView.tsx
+++ client/components/login/SignupView.tsx
@@ -12,17 +12,10 @@ const SignupView = ({...props}) => {
     const [username, setusername] = useState("")
     const [email, setemail] = useState("")
     const [password, setpassword] = useState("")
-    const [statusMessage,setStatusMessage]=useState("")
+    const [statusMessage, setStatusMessage]=useState("")
 
     var newUser : UserInput = {name:name, username:username, password:password, email:email, token:'', }
 
-    // const registered = {
-    //     name:name,
-    //     username:username,
-    //     email:email,
-    //     password: password
-    // }
-    // console.log("newU:",registered)
 
     useEffect(() => {
         if(successful){
@@ -52,10 +45,11 @@ const SignupView = ({...props}) => {
                 </Row>
                 <Row className="login__input">
                     <ButtonView onClick={() =>signUp(newUser)}>Create account</ButtonView>
-                </Row>
-                <Row className="login__input">
                     <ButtonView onClick={handleToggle}>Back to login</ButtonView>
+
                 </Row>
+                {/* <Row className="login__input">
+                </Row> */}
             </Col>
             : <Row><ButtonView onClick={handleToggle}>Register New</ButtonView></Row>
             
